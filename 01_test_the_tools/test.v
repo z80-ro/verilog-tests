0260c45021c8c28c6e1b9deac574ca24f1072f83
z80-ro <z80rom@gmail.com>
2020-03-13 15:45:00 -0700

    Added a new button/led in PMOD is not available

--- 01_test_the_tools/test.v
+++ 01_test_the_tools/test.v
@@ -1,9 +1,13 @@
 module top (
         input BTN_A,
         input BTN_B,
+        input BTN_USR,
+
+        output LED_RED,
         output LED_GREEN);
 
-        assign LED_GREEN = BTN_A & BTN_B;
+        assign LED_GREEN = !(BTN_A & BTN_B);
+        assign LED_RED = BTN_USR;
 
 endmodule
 
