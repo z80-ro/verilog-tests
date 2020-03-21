3692f4b6e086b40bfe16354d239425eb3038bbb6
z80-ro <z80rom@gmail.com>
2020-03-20 19:38:47 -0700

    Added fpga synthesis part

--- 01_clock_frequency/fpgacounter.v
+++ 01_clock_frequency/fpgacounter.v
@@ -1,8 +1,8 @@
 `default_nettype none
 
-module counter (
+module top (
     input CLK,
-    input RESET,
+    input BTN_USR,
     output wire LED_GREEN4, 
     output wire LED_GREEN5 );
 
@@ -11,8 +11,8 @@ reg [31:0] counter;
 assign LED_GREEN5 = counter[4];
 assign LED_GREEN4 = counter[2];
 
-always @(posedge CLK or negedge RESET) begin
-    if (RESET == 0) 
+always @(posedge CLK or negedge BTN_USR) begin
+    if (BTN_USR == 0) 
         counter <= 0;
     else
         counter <= counter + 1;
